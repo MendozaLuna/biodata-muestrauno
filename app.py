03890fffc6a6221f1de072df17ee0a408762c71e
MendozaLuna <mendozaluna3787@gmail.com>
2026-03-03 18:23:16 -0400

    Update app.py

--- app.py
+++ app.py
@@ -353,8 +353,8 @@ if st.session_state.perfil == 'persona':
                 st.write("---")
                 st.write("### 🏥 Todas las sedes disponibles:")
                 st.dataframe(final[['Nombre', 'Precio', 'Km', 'Direccion']], use_container_width=True, hide_index=True)
-            else: 
-             st.error("No se encontraron sedes operativas para este estudio.")
+            else: # <--- ESTE ES EL ELSE QUE DABA ERROR
+            st.error("No se encontraron sedes operativas para este estudio.")
              except Exception as e: st.error(f"Error: {e}")
 
 # --- 7. CONTENIDO EMPRESA ---
