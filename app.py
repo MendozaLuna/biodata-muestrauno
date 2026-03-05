ab7aefd6456762439fd2480ec7e4b606a5b05286
MendozaLuna <mendozaluna3787@gmail.com>
2026-03-04 20:39:26 -0400

    Update app.py

--- app.py
+++ app.py
@@ -439,7 +439,7 @@ if st.session_state.perfil == 'persona':
             # Usamos asteriscos (*) para que el estudio salga en negrita en WhatsApp
             cuerpo_mensaje = (
                 f"Estimados, gusto en saludarles. Estoy interesado en realizarme el examen de *{est_n}* "
-                f"en su sede de {nombre_sede}. Consulté su presupuesto de ${precio_f} a través de BioData. "
+                f"en su sede de {nombre_sede}. Consulté su presupuesto de ${precio_f} a través de *BioData.* "
                 f"¿Cuáles son los requisitos previos o preparación necesaria para este estudio?"
             )
             
