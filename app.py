d7a6d521a7830d87783fa4699db8748a0a446c64
MendozaLuna <mendozaluna3787@gmail.com>
2026-03-12 19:02:30 -0400

    Update app.py

--- app.py
+++ app.py
@@ -498,17 +498,19 @@ if st.session_state.get('sede_seleccionada') is not None:
     lat_dest = mostrar.get('Latitud', 10.4806)
     lon_dest = mostrar.get('Longitud', -66.9036)
 
-    # 2. Renderizado Visual de la Tarjeta
+    tipo_aliado = f"Aliado {plan_raw.replace('Plan', '').strip()}"
+
+    # 2. Renderizado Visual de la Tarjeta XL (Actualizada)
     st.markdown(f"""
         <div style="border: 5px solid {color_tema}; padding: 35px; border-radius: 25px; background-color: white; color: black; margin-top: 10px; text-align: center; box-shadow: 0 10px 25px rgba(0,0,0,0.1);">
             <span style="background-color: {color_tema}; color: white; padding: 6px 18px; border-radius: 12px; font-size: 0.9rem; font-weight: 800; text-transform: uppercase;">
-                Plan {plan_raw}
+                {tipo_aliado}
             </span>
             <h2 style="margin: 20px 0 10px 0; color: #004D40; font-size: 2.3rem; font-weight: 900;">{nombre_clinica}</h2>
             <div style="width: 80px; height: 4px; background-color: {color_tema}; margin: 15px auto 25px auto;"></div>
             <p style="font-size: 1.5rem; margin: 0; color: #101828; font-weight: 500;">
                 📍 <b>Ubicación:</b> {mostrar.get('Dirección', 'Ver mapa abajo')}<br>
-                💰 <b>Presupuesto:</b> ${precio_f}<br>
+                💰 <b>Valor:</b> ${precio_f}<br>
                 📝 <b>Estudio:</b> {est_n}
             </p>
         </div>
